684022a3f5016060066bebb4a87aed16d4a8bbd4
emirkugic <emir.kugic@hotmail.com>
2024-01-16 18:06:11 +0100

    balance page update

--- src/pages/BalancePage.tsx
+++ src/pages/BalancePage.tsx
@@ -1,68 +1,40 @@
 import React from "react";
 import { Typography, Button, Paper, Grid, Container } from "@mui/material";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import CreditCard from "../components/CreditCard";
+import Balance from "../components/Balance";
+
+const scrollableDivStyle = {
+	flex: 1,
+	overflowY: "auto",
+	padding: "20px",
+	position: "fixed",
+	top: 0,
+	bottom: 0,
+	left: 0,
+	scrollbarWidth: "none", // Hide scrollbar on Firefox
+	msOverflowStyle: "none", // Hide scrollbar on Internet Explorer
+};
 
 const BalancePage: React.FC = () => {
 	return (
-		<Container maxWidth="xs">
-			<Paper
-				elevation={3}
-				style={{ padding: "20px", textAlign: "center", marginTop: "20px" }}
-			>
-				<AccountBalanceWalletIcon style={{ fontSize: 40, color: "#003087" }} />
-				<Typography variant="h5" style={{ margin: "10px 0" }}>
-					Total Balance
-				</Typography>
-				<Typography variant="h3" style={{ marginBottom: "10px" }}>
-					0,00 USD
-				</Typography>
-				<Typography variant="subtitle1" style={{ marginBottom: "20px" }}>
-					Estimated total of all currencies
-				</Typography>
-				<Button variant="contained" color="primary" fullWidth>
-					Send payments
-				</Button>
-				<Grid container spacing={2} style={{ marginTop: "20px" }}>
-					<Grid
-						item
-						xs={12}
-						style={{
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "space-between",
-						}}
-					>
-						<span>Cash Balance</span>
-						<Typography variant="body1">USD 0,00</Typography>
-					</Grid>
-					<Grid
-						item
-						xs={12}
-						style={{
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "space-between",
-						}}
-					>
-						<span>Card Balance</span>
-						<Typography variant="body1">EUR 0,00</Typography>
-					</Grid>
-				</Grid>
-				<Button fullWidth style={{ marginTop: "20px" }}>
-					Add an income
-				</Button>
-				<Button fullWidth style={{ marginTop: "10px" }}>
-					Add an expense
-				</Button>
-				<Typography
-					variant="caption"
-					display="block"
-					style={{ marginTop: "20px" }}
-				>
-					Sending money to users feature is coming soon
-				</Typography>
-			</Paper>
-		</Container>
+		<>
+			<div style={{ display: "flex" }}>
+				<div style={scrollableDivStyle}>
+					<CreditCard />
+					<CreditCard />
+					<CreditCard />
+					<CreditCard />
+					<CreditCard />
+					<CreditCard />
+					<CreditCard />
+					<CreditCard />
+				</div>
+				<div style={{ flex: 1, marginLeft: "auto", padding: "20px" }}>
+					<Balance />
+				</div>
+			</div>
+		</>
 	);
 };
 
