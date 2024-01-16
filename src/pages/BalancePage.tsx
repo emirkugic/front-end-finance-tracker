26a5ca4219d3170aedeae062faadf23ad5998666
emirkugic <emir.kugic@hotmail.com>
2024-01-16 15:28:43 +0100

    balance page WIP

--- src/pages/BalancePage.tsx
+++ src/pages/BalancePage.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
+import CreditCard from "../components/credit_card/CreditCard";
 
 const BalancePage = () => {
 	return (
 		<Box sx={{ flexGrow: 1, padding: 2 }}>
 			<div>
 				<h1>Balance Page</h1>
+				<CreditCard />
 			</div>
 		</Box>
 	);
