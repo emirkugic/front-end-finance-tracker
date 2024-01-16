650a8c0e56cee47a1299593eb123000dd2516230
emirkugic <emir.kugic@hotmail.com>
2024-01-16 19:12:20 +0100

    balance page is done

--- src/pages/BalancePage.tsx
+++ src/pages/BalancePage.tsx
@@ -1,39 +1,37 @@
 import React from "react";
-import { Typography, Button, Paper, Grid, Container } from "@mui/material";
-import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import { Grid, Box } from "@mui/material"; // Import Box for adding padding
 import CreditCard from "../components/CreditCard";
 import Balance from "../components/Balance";
-
-const scrollableDivStyle = {
-	flex: 1,
-	overflowY: "auto",
-	padding: "20px",
-	position: "fixed",
-	top: 0,
-	bottom: 0,
-	left: 0,
-	scrollbarWidth: "none", // Hide scrollbar on Firefox
-	msOverflowStyle: "none", // Hide scrollbar on Internet Explorer
-};
 
 const BalancePage: React.FC = () => {
 	return (
 		<>
-			<div style={{ display: "flex" }}>
-				<div style={scrollableDivStyle}>
+			<Box pb={2}>
+				<Balance />
+			</Box>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={6} md={4} lg={3}>
 					<CreditCard />
+				</Grid>
+				<Grid item xs={12} sm={6} md={4} lg={3}>
 					<CreditCard />
+				</Grid>
+				<Grid item xs={12} sm={6} md={4} lg={3}>
 					<CreditCard />
+				</Grid>
+				<Grid item xs={12} sm={6} md={4} lg={3}>
 					<CreditCard />
+				</Grid>
+				<Grid item xs={12} sm={6} md={4} lg={3}>
 					<CreditCard />
+				</Grid>
+				<Grid item xs={12} sm={6} md={4} lg={3}>
 					<CreditCard />
+				</Grid>
+				<Grid item xs={12} sm={6} md={4} lg={3}>
 					<CreditCard />
-					<CreditCard />
-				</div>
-				<div style={{ flex: 1, marginLeft: "auto", padding: "20px" }}>
-					<Balance />
-				</div>
-			</div>
+				</Grid>
+			</Grid>
 		</>
 	);
 };
