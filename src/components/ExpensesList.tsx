183ba9553267a5fedcfbd37f7bece9f112a57737
emirkugic <emir.kugic@hotmail.com>
2024-01-16 15:28:33 +0100

    minor change

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { API_URL } from "../constants"; // Replace with your actual import path
+import { API_URL } from "../constants";
 import {
 	Table,
 	TableBody,
