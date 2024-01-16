abb15ab9085bef11d82f7a44af3399ef5bc025ce
emirkugic <emir.kugic@hotmail.com>
2024-01-16 14:52:48 +0100

    created balance page

---
+++ src/pages/BalancePage.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+const BalancePage = () => {
+	return (
+		<Box sx={{ flexGrow: 1, padding: 2 }}>
+			<div>
+				<h1>Balance Page</h1>
+			</div>
+		</Box>
+	);
+};
+
+export default BalancePage;
