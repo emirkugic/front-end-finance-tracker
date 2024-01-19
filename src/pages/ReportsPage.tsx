c67178f9ff448eeb4f3ac848f394707337c68d6b
emirkugic <emir.kugic@hotmail.com>
2024-01-19 19:32:29 +0100

    reports page now has income pie charts

--- src/pages/ReportsPage.tsx
+++ src/pages/ReportsPage.tsx
@@ -1,41 +1,89 @@
-import { Grid, Paper, Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Grid, Paper, Box, Typography } from "@mui/material";
 import ExpensesPieGraph from "../components/ExpensesPieGraph/ExpensesPieGraph";
 import CombinedDataList from "../components/CombinedDataList";
+import useAuthToken from "../hooks/useAuthToken";
+import IncomePie from "../components/IncomePie";
 
 const ReportsPage = () => {
+	const token = useAuthToken();
+	const [userId, setUserId] = useState("");
+
+	useEffect(() => {
+		if (token) {
+			const tokenPayload = JSON.parse(atob(token.split(".")[1]));
+			setUserId(tokenPayload.userId);
+		}
+	}, [token]);
+
 	return (
 		<>
 			<Box sx={{ flexGrow: 1, padding: 2 }}>
-				<div>
-					<h1 style={{ color: "black" }}>Reports Page</h1>
-
-					<h4 style={{ color: "black" }}>
-						combine the two lists into one big one showing the user his
-						spendings and earnings in chronological order. only have start and
-						end date and have the ability to sort ascending or descending order.
-						name this component ActivityList.tsx and use rafce for this
-						component. TLDR: make a component that will list spending and
-						earnings in one list together
-					</h4>
-
-					<CombinedDataList
-						userId={"6549031fc02fd9204841b306"}
-						startDate="2020-01-01"
-						endDate="2100-01-01"
-					/>
-				</div>
+				<Typography
+					variant="h4"
+					gutterBottom
+					component="div"
+					sx={{ marginBottom: 4 }}
+				>
+					Reports Page
+				</Typography>
 				<Grid container spacing={2} alignItems="center" justifyContent="center">
-					<Grid item xs={12} md={6}>
+					<Grid item xs={12} md={4}>
 						<Paper
 							elevation={0}
 							sx={{ padding: 2, backgroundColor: "transparent" }}
-						></Paper>
+						>
+							<Typography variant="h6" gutterBottom>
+								Your income is received through
+							</Typography>
+							<IncomePie
+								startDate="2020-01-01"
+								endDate="2100-01-01"
+								groupBy="receivedThrough"
+								userId={userId}
+							/>
+						</Paper>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<Paper
+							elevation={0}
+							sx={{ padding: 2, backgroundColor: "transparent" }}
+						>
+							<Typography variant="h6" gutterBottom>
+								Your main income comes from
+							</Typography>
+							<IncomePie
+								startDate="2020-01-01"
+								endDate="2100-01-01"
+								groupBy="source"
+								userId={userId}
+							/>
+						</Paper>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<Paper
+							elevation={0}
+							sx={{ padding: 2, backgroundColor: "transparent" }}
+						>
+							<Typography variant="h6" gutterBottom>
+								Your main income providers
+							</Typography>
+							<IncomePie
+								startDate="2020-01-01"
+								endDate="2100-01-01"
+								groupBy="from"
+								userId={userId}
+							/>
+						</Paper>
 					</Grid>
 					<Grid item xs={12} md={6}>
 						<Paper
 							elevation={0}
 							sx={{ padding: 2, backgroundColor: "transparent" }}
 						>
+							<Typography variant="h6" gutterBottom>
+								You spent your money on
+							</Typography>
 							<ExpensesPieGraph
 								startDate="2020-01-01"
 								endDate="2100-01-01"
@@ -48,12 +96,22 @@ const ReportsPage = () => {
 							elevation={0}
 							sx={{ padding: 2, backgroundColor: "transparent" }}
 						>
+							<Typography variant="h6" gutterBottom>
+								Your preferred payment method
+							</Typography>
 							<ExpensesPieGraph
 								startDate="2020-01-01"
 								endDate="2100-01-01"
 								parameter="source"
 							/>
 						</Paper>
+					</Grid>
+					<Grid item xs={12}>
+						<CombinedDataList
+							userId={userId}
+							startDate="2020-01-01"
+							endDate="2100-01-01"
+						/>
 					</Grid>
 				</Grid>
 			</Box>
