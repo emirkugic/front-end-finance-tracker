5fe23e072b813ef1a2f1220ef58f80c559a88d0b
emirkugic <emir.kugic@hotmail.com>
2024-01-19 19:33:10 +0100

    balance page now uses jwt id, instead of hard coded id

--- src/pages/BalancePage.tsx
+++ src/pages/BalancePage.tsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Box, Fab, useTheme } from "@mui/material";
 import CreditCard from "../components/CreditCard";
 import Balance from "../components/Balance";
 import AddIcon from "@mui/icons-material/Add";
 import useCreditCardDetails from "../hooks/useCreditCardDetails";
 import CreateCardModal from "../components/CreateCardModal";
+import useAuthToken from "../hooks/useAuthToken";
 
 const BalancePage: React.FC = () => {
 	const [openModal, setOpenModal] = useState(false);
 	const theme = useTheme();
-	const userId = "6549031fc02fd9204841b306";
+	// const userId = "6549031fc02fd9204841b306";
+
+	const token = useAuthToken();
+	const [userId, setUserId] = useState("");
+
+	useEffect(() => {
+		if (token) {
+			const tokenPayload = JSON.parse(atob(token.split(".")[1]));
+			setUserId(tokenPayload.userId);
+		}
+	}, [token]);
+
 	const { cards, loading, error } = useCreditCardDetails(userId);
 
 	const handleOpenModal = () => {
