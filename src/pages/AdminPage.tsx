75eb8a0ea88b6825353ae614bd2c318a39cdd6d6
emirkugic <emir.kugic@hotmail.com>
2024-01-21 22:26:11 +0100

    checks if user is admin to let access

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuthToken from "../hooks/useAuthToken";
+import useFetchUserData from "../hooks/useFetchUserData";
 import ListUsers from "../components/ListUsers";
+import { jwtDecode } from "jwt-decode";
 
 const AdminPage = () => {
+	const token = useAuthToken();
+	const navigate = useNavigate();
+	let userId = null;
+
+	if (token) {
+		const decoded = jwtDecode(token);
+		userId = decoded.userId;
+	}
+
+	const { userData } = useFetchUserData(userId);
+
+	useEffect(() => {
+		let isAdmin = false;
+
+		if (userData) {
+			isAdmin =
+				userData.userType === "ADMIN" ||
+				userData.authorities?.some((auth) => auth.authority === "ADMIN");
+
+			if (!isAdmin) {
+				navigate("/profile");
+			}
+			console.log(userData.userType);
+		}
+	}, [navigate, userData]);
+
 	return (
 		<div>
 			<ListUsers />
