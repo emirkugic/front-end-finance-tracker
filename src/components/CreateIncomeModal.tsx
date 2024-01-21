36d224c6b69af7c0bc178a47756a93558e9bfda8
emirkugic <emir.kugic@hotmail.com>
2024-01-21 22:25:18 +0100

    when creating income a list of cards is presented

--- src/components/CreateIncomeModal.tsx
+++ src/components/CreateIncomeModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {
 	Box,
@@ -8,32 +8,29 @@ import {
 	DialogContent,
 	DialogTitle,
 	Fab,
+	Select,
+	MenuItem,
 } from "@mui/material";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import { LocalizationProvider, DateTimePicker } from "@mui/lab";
 import AddIcon from "@mui/icons-material/Add";
 import { API_URL } from "../constants";
 import useAuthToken from "../hooks/useAuthToken";
+import useFetchCreditCards from "../hooks/useFetchCreditCards"; // Import the custom hook
 
 const CreateIncomeModal = ({ onClose }) => {
 	const [amount, setAmount] = useState(0);
 	const [source, setSource] = useState("");
-	const [receivedThrough, setReceivedThrough] = useState("");
+	const [receivedThrough, setReceivedThrough] = useState("Cash"); // Default to 'Cash'
 	const [from, setFrom] = useState("");
 	const [receivedDate, setReceivedDate] = useState(new Date());
 	const [open, setOpen] = useState(false);
 
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
-
-	const handleClose = () => {
-		setOpen(false);
-		onClose();
-	};
-
 	const token = useAuthToken();
 	const [userId, setUserId] = useState("");
+
+	// Fetch credit cards using the custom hook
+	const { creditCards, loading, error } = useFetchCreditCards(userId);
 
 	useEffect(() => {
 		if (token) {
@@ -61,8 +58,20 @@ const CreateIncomeModal = ({ onClose }) => {
 		}
 	};
 
+	const handleReceivedThroughChange = (event) => {
+		setReceivedThrough(event.target.value);
+	};
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+	const handleClose = () => {
+		setOpen(false);
+		if (onClose) onClose();
+	};
+
 	return (
-		<div div style={{ position: "fixed", bottom: "16px", right: "16px" }}>
+		<div style={{ position: "fixed", bottom: "16px", right: "16px" }}>
 			<Fab color="primary" aria-label="add" onClick={handleClickOpen}>
 				<AddIcon />
 			</Fab>
@@ -88,14 +97,22 @@ const CreateIncomeModal = ({ onClose }) => {
 							value={source}
 							onChange={(e) => setSource(e.target.value)}
 						/>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
+						<Select
 							label="Received Through"
 							value={receivedThrough}
-							onChange={(e) => setReceivedThrough(e.target.value)}
-						/>
+							onChange={handleReceivedThroughChange}
+							fullWidth
+							margin="normal"
+							displayEmpty
+						>
+							<MenuItem value="Cash">Cash</MenuItem>
+							{creditCards &&
+								creditCards.map((card) => (
+									<MenuItem key={card.id} value={card.id}>
+										{card.cardName}
+									</MenuItem>
+								))}
+						</Select>
 						<TextField
 							margin="normal"
 							required
